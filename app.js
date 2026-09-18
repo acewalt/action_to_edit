e2ae011e8ca26a4cdff9c9e5f349b30146bfbc52
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-18 12:08:57 -0500

    Refresh improved retarget auto-match

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import {
   countValidPairs as countValidRetargetPairs,
   sortPairsStandard as sortRetargetPairsStandard,
   buildRetargetClip,
-} from './retargeting.js?v=20260918-34';
+} from './retargeting.js?v=20260918-35';
 import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-31';
 
 const $ = (selector) => document.querySelector(selector);
