5e83b596dc329cbb6fada16497f1b832f7bcc13c
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-18 12:50:38 -0500

    Refresh normalized rest-pose editor v41

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ import {
   countValidPairs as countValidRetargetPairs,
   sortPairsStandard as sortRetargetPairsStandard,
   buildRetargetClip,
-} from './retargeting.js?v=20260918-40';
-import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-40';
+} from './retargeting.js?v=20260918-41';
+import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-41';
 
 const $ = (selector) => document.querySelector(selector);
 
