1ac10c762ec75b595baaa4e551b9b2dc389e0a50
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-18 14:09:01 -0500

    Publish bind-pose retarget build v47

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ import {
   countValidPairs as countValidRetargetPairs,
   sortPairsStandard as sortRetargetPairsStandard,
   buildRetargetClip,
-} from './retargeting.js?v=20260918-46';
-import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-46';
+} from './retargeting.js?v=20260918-47';
+import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-47';
 
 const $ = (selector) => document.querySelector(selector);
 
