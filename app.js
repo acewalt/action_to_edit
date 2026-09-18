2dfe9357fa8ad2f3f7be3fea788c70ded59520b6
walt_to_art <guard.mauricio.save@gmail.com>
2026-09-18 11:53:19 -0500

    Refresh retarget bone-name resolver

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import {
   countValidPairs as countValidRetargetPairs,
   sortPairsStandard as sortRetargetPairsStandard,
   buildRetargetClip,
-} from './retargeting.js?v=20260918-31';
+} from './retargeting.js?v=20260918-33';
 import { RestPoseEditor } from './rest-pose-editor.js?v=20260918-31';
 
 const $ = (selector) => document.querySelector(selector);
